e568e97f8f21354daf45b97850008156b6be816a
James Bronzan <jbronzan@gmail.com>
2016-10-17 16:53:12 -0400

    update yearly_averages.r
    Now runs through multiple models

--- yearly_averages.r
+++ yearly_averages.r
@@ -3,7 +3,6 @@
 options(stringsAsFactors = FALSE)
 modelnames <- c("access1-0","bcc-csm1-1-m","bcc-csm1-1","canesm2","ccsm4","cesm1-bgc","cesm1-cam5","cmcc-cm","cnrm-cm5","csiro-mk3-6-0","fgoals-g2","fio-esm","gfdl-cm3","gfdl-esm2g","gfdl-esm2m","giss-e2-r","hadgem2-ao","hadgem2-cc","hadgem2-es","inmcm4","ipsl-cm5a-mr","ipsl-cm5b-lr","miroc-esm-chem","miroc-esm","miroc5_rcp85","mpi-esm-lr","mpi-esm-mr","mri-cgcm3","noresm1-m")
 models <- c("access1-0_rcp85_r1i1p1","bcc-csm1-1-m_rcp85_r1i1p1","bcc-csm1-1_rcp85_r1i1p1","canesm2_rcp85_r1i1p1","ccsm4_rcp85_r1i1p1","cesm1-bgc_rcp85_r1i1p1","cesm1-cam5_rcp85_r1i1p1","cmcc-cm_rcp85_r1i1p1","cnrm-cm5_rcp85_r1i1p1","csiro-mk3-6-0_rcp85_r1i1p1","fgoals-g2_rcp85_r1i1p1","fio-esm_rcp85_r1i1p1","gfdl-cm3_rcp85_r1i1p1","gfdl-esm2g_rcp85_r1i1p1","gfdl-esm2m_rcp85_r1i1p1","giss-e2-r_rcp85_r1i1p1","hadgem2-ao_rcp85_r1i1p1","hadgem2-cc_rcp85_r1i1p1","hadgem2-es_rcp85_r1i1p1","inmcm4_rcp85_r1i1p1","ipsl-cm5a-mr_rcp85_r1i1p1","ipsl-cm5b-lr_rcp85_r1i1p1","miroc-esm-chem_rcp85_r1i1p1","miroc-esm_rcp85_r1i1p1","miroc5_rcp85_r1i1p1","mpi-esm-lr_rcp85_r1i1p1","mpi-esm-mr_rcp85_r1i1p1","mri-cgcm3_rcp85_r1i1p1","noresm1-m_rcp85_r1i1p1")
-modelname <- modelnames[1]  ## here is where you would adjust to iterate through models
 
 # construct dates
 
@@ -16,8 +15,39 @@ for (m in 1:12) {
 	ydates <- c(ydates, mdates)
 }
 
+# initialize annual average
+
+
+writenamer <- function(model, yr, mtype) {
+	basename <- paste0("~/rawdata/",model,"/")
+	name <- paste0(basename,"conus_c5.",model,".daily.",mtype,".",yr,".nc");
+}
+
+
+year <- "2100" ## Here is where you would adjust to iterate through 20-year periods
+
+for (mod in 1:length(list.files("tasmax"))) {
+# loop through dates, assemble data frame of daily averages per year.
+model <- models[mod]
+fn <- writenamer(model, year, "tasmax")
+
+modelname <- modelnames[mod]  ## here is where you would adjust to iterate through models
+annualavg <- df[,1:3]
+date_average_all_models <- df[,1:3]
+fn <- 
+
+
 for (d in ydates) {
-	dailyavg <- df[,substr(colnames(df),6,10) == d]
-	save(dailyavg, file="daily_avg_", modelname, "_", d, ".Rdata")
+	print(paste(d, "at", Sys.time()))
+	dfsubset <- df[,substr(colnames(df),6,10) == d]
+	date_average <- rowMeans(dfsubset)
+	annualavg <- cbind(annualavg, date_average)
+	colnames(annualavg)[length(colnames(annualavg))] <- paste0("x", d)
+
+
+}
+
+annualavg <- annualavg[,2:ncol(annualavg)]
+save(annualavg, file=paste0("annual_profile_", year, "_", modelname, ".", "RData"))
 
 }
